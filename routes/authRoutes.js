1915ca668ffcd65abb27f0b05611a7ac20512159
Adem Mohammed <kingnanfo@gmail.com>
2025-04-28 00:42:29 +0300

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.get('/verify/:token', verifyEmail);
 router.post('/resend-verification', resendVerification);
-router.post('/request-password-reset', requestPasswordReset);
+router.post('/auth/request-password-reset', requestPasswordReset);
 router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
