57c559d94fb80e11f392bf7153410b09834c687a
Adem Mohammed <kingnanfo@gmail.com>
2025-04-28 00:50:13 +0300

    updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use(cors());
