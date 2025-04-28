72a23b172912d79996e1e28673d192a8d05c5392
Adem Mohammed <kingnanfo@gmail.com>
2025-04-28 22:49:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -35,12 +35,10 @@ app.get('*', (req, res) => {
 });
 
 mongoose
-    .connect('mongodb+srv://adem:adem@np.g88gat7.mongodb.net/?retryWrites=true&w=majority&appName=np', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+    .connect('mongodb+srv://adem:adem@np.g88gat7.mongodb.net/?retryWrites=true&w=majority&appName=np')
+
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
